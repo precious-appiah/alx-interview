9c46adea2a086059d87c6cb26c2c266ece849d3f
Awura Precious <aaprecious99@gmail.com>
2024-08-12 17:56:56 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+def pascal_triangle(n):    
+    """Initialize the triangle with the first row"""
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1) 
+        triangle.append(row)
+    
+    return triangle
+
