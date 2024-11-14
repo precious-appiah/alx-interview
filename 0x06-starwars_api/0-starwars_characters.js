e2747fcd6094bd950336c23e9bc8bb903b2bc5fc
Awura Precious <aaprecious99@gmail.com>
2024-11-14 19:01:49 +0000

    swapi

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+//extract the value 3 from it
+
+const request = require("request");
+const args = process.argv[2];
+
+let res = [];
+
+request(
+  `https://swapi-api.alx-tools.com/api/films/${args}`,
+  { json: true },
+  function (error, response, body) {
+    res = body.characters;
+    // console.log("body:", res); // Print the HTML for the Google homepage.
+
+    //loop through the array
+    //make an api call
+    //get the name of every object
+    const names = res.map((item) => {
+      request(item, { json: true }, (error, response, body) => {
+        console.log(body.name);
+      });
+    });
+  }
+);
