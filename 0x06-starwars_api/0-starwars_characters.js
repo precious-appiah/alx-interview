63a1d568dd63e1e08b8061740863c17eb66827d2
Awura Precious <aaprecious99@gmail.com>
2024-11-14 19:14:43 +0000

    swapi fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
-const request = require("request");
+const request = require('request');
 const args = process.argv[2];
 
 let res = [];
@@ -9,14 +9,16 @@ request(
   `https://swapi-api.alx-tools.com/api/films/${args}`,
   { json: true },
   function (error, response, body) {
+    if (error) return error;
     res = body.characters;
     // console.log("body:", res); // Print the HTML for the Google homepage.
 
-    //loop through the array
-    //make an api call
-    //get the name of every object
-    const names = res.map((item) => {
-      request(item, { json: true }, (error, response, body) => {
+    // loop through the array
+    // make an api call
+    // get the name of every object
+    res.map((item) => {
+      return request(item, { json: true }, (error, response, body) => {
+        if (error) return error;
         console.log(body.name);
       });
     });
